7563b27cd184a56f8af106f1ca424acceb0348d1
Carl Lundin <lundinc@amazon.com>
2020-11-05 16:51:34 -0800

    Add first proof.

---
+++ test/cbmc/proofs/PKI_mbedTLSSignatureToPkcs11Signature/PKI_mbedTLSSignatureToPkcs11Signature_harness.c
@@ -0,0 +1,25 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file PKI_mbedTLSSignatureToPkcs11Signature_harness.c
+ * @brief Implements the proof harness for PKI_mbedTLSSignatureToPkcs11Signature function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+
+void harness()
+{
+
+  /* Insert argument declarations */
+
+  PKI_mbedTLSSignatureToPkcs11Signature( /* Insert arguments */ );
+}
