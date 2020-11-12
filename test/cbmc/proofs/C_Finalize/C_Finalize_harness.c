86ef3085fee86289c479604154012900e0fb5c4d
Carl Lundin <lundinc@amazon.com>
2020-11-11 19:21:29 -0800

    Finish C_Finalize proof.

--- test/cbmc/proofs/C_Finalize/C_Finalize_harness.c
+++ test/cbmc/proofs/C_Finalize/C_Finalize_harness.c
@@ -1,8 +1,26 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 /*
- * Insert copyright notice
+ * corePKCS11 V2.0.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
  */
 
 /**
@@ -10,16 +28,14 @@
  * @brief Implements the proof harness for C_Finalize function.
  */
 
-/*
- * Insert project header files that
- *   - include the declaration of the function
- *   - include the types needed to declare function arguments
- */
+#include <stddef.h>
+#include "core_pkcs11.h"
 
 void harness()
 {
+    CK_RV xResult = C_Initialize( NULL );
+    __CPROVER_assert( xResult <= CKR_OK, "PKCS #11 module needs to be initialized"
+          " to be uninitialized." );
 
-  /* Insert argument declarations */
-
-  C_Finalize( /* Insert arguments */ );
+  ( void ) C_Finalize( NULL );
 }
