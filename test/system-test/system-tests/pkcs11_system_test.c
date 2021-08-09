bedd302e8c8a267f3a9b686dec6495b7b7a0b9b0
Gaurav Aggarwal <aggarg@amazon.com>
2021-08-09 20:04:06 +0000

    Fix formatting
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- test/system-test/system-tests/pkcs11_system_test.c
+++ test/system-test/system-tests/pkcs11_system_test.c
@@ -1935,13 +1935,13 @@ static CK_RV provisionPublicKey( CK_SESSION_HANDLE session,
                                              NULL, 0 );
         CK_ATTRIBUTE publicKeyTemplate[] =
         {
-            { CKA_CLASS,           NULL /* &class */,           sizeof( CK_OBJECT_CLASS )                 },
-            { CKA_KEY_TYPE,        NULL /* &publicKeyType */,   sizeof( CK_KEY_TYPE )                     },
-            { CKA_TOKEN,           NULL /* &trueObject */,      sizeof( trueObject )                      },
-            { CKA_MODULUS,         NULL /* &( modulus[ 1 ] ) */,MODULUS_LENGTH                            },          /* Extra byte allocated at beginning for 0 padding. */
-            { CKA_VERIFY,          NULL /* &trueObject */,      sizeof( trueObject )                      },
-            { CKA_PUBLIC_EXPONENT, NULL /* publicExponent */,   sizeof( publicExponent )                  },
-            { CKA_LABEL,           publicKeyLabel,              strlen( ( const char * ) publicKeyLabel ) }
+            { CKA_CLASS,           NULL /* &class */,            sizeof( CK_OBJECT_CLASS )                 },
+            { CKA_KEY_TYPE,        NULL /* &publicKeyType */,    sizeof( CK_KEY_TYPE )                     },
+            { CKA_TOKEN,           NULL /* &trueObject */,       sizeof( trueObject )                      },
+            { CKA_MODULUS,         NULL /* &( modulus[ 1 ] ) */, MODULUS_LENGTH                            },         /* Extra byte allocated at beginning for 0 padding. */
+            { CKA_VERIFY,          NULL /* &trueObject */,       sizeof( trueObject )                      },
+            { CKA_PUBLIC_EXPONENT, NULL /* publicExponent */,    sizeof( publicExponent )                  },
+            { CKA_LABEL,           publicKeyLabel,               strlen( ( const char * ) publicKeyLabel ) }
         };
 
         /* Aggregate initializers must not use the address of an automatic variable. */
