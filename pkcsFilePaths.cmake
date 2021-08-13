68a96addd46655e05bc168bd9599f015c1737332
Archit Gupta <71798289+archigup@users.noreply.github.com>
2021-08-13 13:30:46 -0700

    Fix licenses and windows path (#131)

--- pkcsFilePaths.cmake
+++ pkcsFilePaths.cmake
@@ -18,14 +18,14 @@ set( PKCS_INCLUDE_PUBLIC_DIRS
      "${CMAKE_CURRENT_LIST_DIR}/source/include"
      )
 
-# corePKCS11 PAL posix source files.
+# corePKCS11 PAL Posix source files.
 set( PKCS_PAL_POSIX_SOURCES
      "${CMAKE_CURRENT_LIST_DIR}/source/portable/os/core_pkcs11_pal_utils.c"
      "${CMAKE_CURRENT_LIST_DIR}/source/portable/os/posix/core_pkcs11_pal.c"
      )
 
-# corePKCS11 PAL posix source files.
-set( PKCS_PAL_POSIX_SOURCES
+# corePKCS11 PAL Windows source files.
+set( PKCS_PAL_WINDOWS_SOURCES
      "${CMAKE_CURRENT_LIST_DIR}/source/portable/os/core_pkcs11_pal_utils.c"
      "${CMAKE_CURRENT_LIST_DIR}/source/portable/os/freertos_winsim/core_pkcs11_pal.c"
      )
